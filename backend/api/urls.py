7fb874ce9958656eddd1c89d268ef503255c3336
HITMANek210 <f.wyrembak@poczta.fm>
2024-05-31 13:38:44 +0200

    Added tests for authentication

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+from rest_framework.authtoken import views as auth_views
+
 urlpatterns = [
-    path('get_users/', view=views.get_users.as_view(), name="get_users_page"),
+    path('get_token/', view=auth_views.obtain_auth_token, name="get_token_page"),
+    path('get_users/', view=views.GetUsersView.as_view(), name="get_users_page"),
+    path('test_token/', view=views.TestTokenView.as_view(), name="test_token_page"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework import generics, authentication, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
 
 from . serializers import UserSerializer
 
 # Create your views here.
 
-class get_users(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class GetUsersView(APIView):
+    authentication_classes = [authentication.SessionAuthentication, authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+
+        return Response(serializer.data)
+    
+class TestTokenView(APIView):
+    authentication_classes = [authentication.SessionAuthentication, authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, format=None):
+        return Response(f"Token passed for: {request.user.username}")
