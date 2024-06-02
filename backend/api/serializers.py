b74c989567767874976537052c8421d60bfcd8e8
HITMANek210 <f.wyrembak@poczta.fm>
2024-06-02 22:24:48 +0200

    Added more fields to UserSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    group_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ["id", "username", "is_superuser"]
+        fields = ["username", "email", "first_name", "last_name", "is_superuser", "is_staff", "is_active", "group_name"]
+
+    def get_group_name(self, obj):
+        return getattr(User.objects.get(username=obj.username).groups.first(), "name", "users")
