bd68f25c80f058afb8b48e3bfba9826802641a34
HITMANek210 <f.wyrembak@poczta.fm>
2024-06-21 00:51:47 +0200

    Fixed UserSerializer for editing passwords

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -12,27 +12,32 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "password", "email", "first_name", "last_name", "is_superuser", "is_staff", "is_active", "group_name"]
         extra_kwargs = {
                 'id': {'read_only': True},
-                'password': {'write_only': True},
+                'password': {'write_only': True, "required": False},
         }
     
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            is_superuser=validated_data['is_superuser'],
-            is_staff=validated_data['is_staff'],
-            group_name=validated_data['group_name']
-        )
+        try:
+            user = User.objects.create_user(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validated_data['password'],
+                first_name=validated_data['first_name'],
+                last_name=validated_data['last_name'],
+                is_superuser=validated_data['is_superuser'],
+                is_staff=validated_data['is_staff'],
+                group_name=validated_data['group_name']
+            )
+        except Exception as e:
+            error = {'error': ",".join(e.args)}
+            raise serializers.ValidationError(error)
 
         return user
 
     def update(self, instance, validated_data):
         user = User.objects.get(username=validated_data['username'])
 
-        user.set_password(validated_data['password'])
+        if "password" in validated_data:
+            user.set_password(validated_data['password'])
 
         user.email=validated_data['email']
         user.first_name=validated_data['first_name']
